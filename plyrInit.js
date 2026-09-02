61a42d15aa2fd0c42decccfb911edd40cb502186
MzGit <46912696+mzrepo@users.noreply.github.com>
2026-09-02 16:34:15 -0400

    Update plyrInit.js

--- plyrInit.js
+++ plyrInit.js
@@ -1,2 +1,25 @@
 //Plyr Initialize
 const players = Array.from(document.querySelectorAll('.js-player')).map((p) => new Plyr(p));
+
+//SPLIDE SLIDESHOW
+var splide = new Splide('.splide', {
+    type: 'loop',
+    perPage: 1,
+    autoplay: true,
+    interval: 5500,
+});
+
+splide.on('moved', function (newIndex) {
+
+    document.querySelectorAll('.slide-content').forEach(btn => {
+        btn.classList.remove('animShow');
+    });
+
+    const activeSlide = splide.Components.Slides.getAt(newIndex);
+
+    activeSlide.slide.querySelector('.slide-content')?.classList.add('animShow');
+
+    console.log(newIndex);
+});
+
+splide.mount();
